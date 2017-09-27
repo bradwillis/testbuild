436b04c66041a9a969ad1e49a2382f3d412ddf75
Brad Willis <brwillis@microsoft.com>
2017-09-27 14:11:06 -0400

    Adding basic project

---
+++ TestBuildArtifacts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBuildArtifacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.Write("This is a test console app.");
+        }
+    }
+}
